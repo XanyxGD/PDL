150ac6441e377cc2c5d5b13db96018a3ad0a997b
XanyxGD <166867552+XanyxGD@users.noreply.github.com>
2024-06-16 13:42:27 +0100

    Update score.js

--- js/score.js
+++ js/score.js
@@ -1,9 +1,5 @@
 /**
  * Numbers of decimal digits to round to
- (-0.909091 * (rank - 1) + 100.909091)
- 100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50
- ((percent - (minPercent - 1)) / (100 - (minPercent - 1))
- -0.939597 * (rank)) + 150.939597
  */
 const scale = 1;
 
@@ -18,15 +14,26 @@ export function score(rank, percent, minPercent) {
     if (rank > 150) {
         return 0;
     }
+    if (rank > 75 && percent < 100) {
+        return 0;
+    }
 
-    let score = ((-0.22371358 * (rank)) + 50.22371358) * ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    // Old formula
+    /*
+    let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
+        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    */
+    // New formula
+    let score = (-24.9975*Math.pow(rank-1, 0.4) + 50) *
+        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+
     score = Math.max(0, score);
 
     if (percent != 100) {
         return round(score - score / 3);
     }
 
-    return round(score);
+    return Math.max(round(score), 0);
 }
 
 export function round(num) {
